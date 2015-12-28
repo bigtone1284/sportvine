3377a49f02ba8bdc5b8cdcdf466eac3b2c9c923f
Tony DeFreitas <anthony.defreitas@gmail.com>
2015-12-27 21:14:49 -0800

    added new nodemon

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ var request = require("request");
     
 var port = process.env.PORT || 3000;
 var app = express();
+
+require('dotenv').load();
+
+app.use(morgan("combined"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.set("view options", {
+  layout: false
+});
+
+app.use(express.static(__dirname + "/public"));
